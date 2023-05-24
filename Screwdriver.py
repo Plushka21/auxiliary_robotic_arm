d7720b3aeebf30021bf014c28cf9664b2c8d4e4b
Anton Buguev <albugg2001@gmail.com>
2023-05-24 09:57:23 +0300

    Added Arduino launch from Python (for Ubuntu)

--- Screwdriver.py
+++ Screwdriver.py
@@ -1,11 +1,13 @@
 import pyfirmata
 import time
+import os
 
 POW_PIN_NUM = 3
 DIR_PIN_NUM = 7
 
 class Screwdriver:
-    def __init__(self, port="COM5", MAX_SPEED=80):
+    def __init__(self, port="/dev/ttyACM0", MAX_SPEED=80):
+        os.system(f"arduino --upload StandardFirmata/StandardFirmata.ino --port {port}")
         self.board = pyfirmata.Arduino(port)
         self.POW_PIN = self.board.get_pin(f'd:{POW_PIN_NUM}:p')
         self.MAX_SPEED = MAX_SPEED
@@ -20,12 +22,16 @@ class Screwdriver:
             self.board.digital[DIR_PIN_NUM].write(direction)
             time.sleep(1)
             for i in range(50, self.MAX_SPEED):
-                # self.POW_PIN.write(i/255)
+                self.POW_PIN.write(i/255)
                 time.sleep(30/1000)
             time.sleep(time_to_run)
             self.POW_PIN.write(0)
+            self.board.digital[DIR_PIN_NUM].write(0)
         else:
             print("ENTER EITHER 0 OR 1 FOR 'direction' ARGUMENT")
             return
-# screw = Screwdriver()
-# screw.turn_on()
+
+screw = Screwdriver()
+screw.turn_on()
+time.sleep(2)
+screw.turn_on(0)
